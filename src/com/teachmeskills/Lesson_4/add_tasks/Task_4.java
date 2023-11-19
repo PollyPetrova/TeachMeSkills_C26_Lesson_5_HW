881fc15ab80543cde7424f247ac145e6dca15ae7
PollyPetrova <polina.petrova.030325@gmail.com>
2023-11-19 17:15:09 +0300

    Second commit
    Lesson_5

--- src/com/teachmeskills/Lesson_4/add_tasks/Task_4.java
+++ src/com/teachmeskills/Lesson_4/add_tasks/Task_4.java
@@ -24,7 +24,7 @@ public class Task_4 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if(i==j){
-                    System.out.print(+array[i][j]+" ");
+                    System.out.print(array[i][j]+" ");
                 }
             }
 
